d3e4fc16ffb9fac7b4216251f82e0a0b3b03df2f
Rudolf Polzer <divVerent@gmail.com>
2023-04-29 17:57:11 -0400

    Allow specifying yards/feet.

--- targets.js
+++ targets.js
@@ -1,7 +1,62 @@
-function toPx(w) {
-  let n = document.getElementById('unit_converter');
-  n.style.width = w;
-  return parseFloat(window.getComputedStyle(n).width);
+function toPx(w, distance) {
+  let result = /^(\d*\.?\d*)(\w+)$/.exec(w);
+  if (result == null) {
+    console.error("could not find unit: " + w);
+    return 0;
+  }
+  let amount = parseFloat(result[1]);
+  let unit = result[2];
+  switch (unit) {
+  case "px":
+    return amount;
+  case "in":
+    return amount * 96;
+  case "pt":
+    return amount * 96 / 72;
+  case "ft":
+    return amount * 96 * 12;
+  case "yd":
+    return amount * 96 * 36;
+  case "mm":
+    return amount * 96 / 25.4;
+  case "cm":
+    return amount * 96 / 25.4 * 10;
+  case "m":
+    return amount * 96 / 25.4 * 1000;
+  case "mil":
+    return amount * distance / 1000;
+  case "moa":
+    return amount * distance / 3600;
+  default:
+    console.error("could not recognize unit: " + w);
+    return 0;
+  }
+}
+
+function nicePrint(val) {
+  if (val >= 100) {
+    return val.toFixed(0);
+  }
+  if (val >= 10) {
+    return val.toFixed(1);
+  }
+  return val.toFixed(2);
+}
+
+// TODO: also accept angles in form 5in@100ft.
+// Then scale them according to caliber.
+function toPxAdjusted(w, scale, distance, caliberFrom, caliberTo) {
+  let result = /^([^@]*)(?:@(.*))?$/.exec(w);
+  if (result == null) {
+    console.error("could not find separation: " + w);
+    return 0;
+  }
+  let baseValue = toPx(result[1], distance);
+  if (result[2] != null) {
+    let baseDistance = toPx(result[2], null);
+    scale *= distance / baseDistance;
+  }
+  return (baseValue + caliberFrom / 2) * scale - caliberTo / 2;
 }
 
 const preamble = `
@@ -9,16 +64,16 @@ macro B-2
 ringcolor black
 innercolor white
 textcolor black
-ring 7.33in 4
-ring 5.56in 5
-ring 4.16in 6
+ring 7.33in@50ft 4@mm
+ring 5.56in@50ft 5
+ring 4.16in@50ft 6
 ringcolor red
 innercolor black
 textcolor white
-ring 3.07in 7
-ring 2.23in 8
-ring 1.54in 9
-ring 0.90in 10
+ring 3.07in@50ft 7
+ring 2.23in@50ft 8
+ring 1.54in@50ft 9
+ring 0.90in@50ft 10
 textcolor black
 end
 
@@ -26,15 +81,15 @@ macro B-3
 ringcolor black
 innercolor white
 textcolor black
-ring 8.32in 6
-ring 6.14in 7
-ring 4.46in 8
+ring 8.32in@50ft 6@mm
+ring 6.14in@50ft 7
+ring 4.46in@50ft 8
 ringcolor red
 innercolor black
 textcolor white
-ring 3.06in 9
-ring 1.80in 10
-ring 0.90in X
+ring 3.06in@50ft 9
+ring 1.80in@50ft 10
+ring 0.90in@50ft X
 textcolor black
 end
 
@@ -42,7 +97,7 @@ macro mil@15m
 ringcolor black
 innercolor white
 textcolor black
-ring 18cm 6mil
+ring 18cm 6mil@mm
 ring 15cm 5mil
 ring 12cm 4mil
 ringcolor red
@@ -58,7 +113,7 @@ macro mil@15m-small
 ringcolor black
 innercolor white
 textcolor black
-ring 6cm 2mil
+ring 6cm 2mil@mm
 ring 4.5cm 1.5mil
 ringcolor red
 innercolor black
@@ -72,7 +127,7 @@ macro mil@25m
 ringcolor black
 innercolor white
 textcolor black
-ring 20cm 4mil
+ring 20cm 4mil@mm
 ring 17.5cm 3.5mil
 ring 15cm 3mil
 ring 12.5cm 2.5mil
@@ -90,7 +145,7 @@ macro mil@50m
 ringcolor black
 innercolor white
 textcolor black
-ring 20cm 2mil
+ring 20cm 2mil@mm
 ring 18cm 1.8mil
 ring 16cm 1.6mil
 ring 14cm 1.4mil
@@ -110,7 +165,7 @@ macro moa@50ft
 ringcolor black
 innercolor white
 textcolor black
-ring 8in 24moa
+ring 8in 24moa@mm
 ring 7in 21moa
 ring 6in 18moa
 ring 5in 15moa
@@ -128,7 +183,7 @@ macro moa@25yd
 ringcolor black
 innercolor white
 textcolor black
-ring 8in 16moa
+ring 8in 16moa@mm
 ring 7in 14moa
 ring 6in 12moa
 ring 5in 10moa
@@ -146,7 +201,7 @@ macro moa@50yd
 ringcolor black
 innercolor white
 textcolor black
-ring 8in 8moa
+ring 8in 8moa@mm
 ring 7in 7moa
 ring 6in 6moa
 ring 5in 5moa
@@ -196,6 +251,7 @@ function render() {
   let ringColor = 'black';
   let innerColor = 'grey';
   let textColor = 'black';
+  let distance = toPx('50ft', null);
 
   let stack = (preamble + str).split(/\r?\n/).reverse();
 
@@ -229,17 +285,17 @@ function render() {
       break;
     }
     case 'unit':
-      unit = toPx(args[1]);
+      unit = toPx(args[1], null);
       break;
     case 'paper':
-      paperX = toPx(args[1]);
-      paperY = toPx(args[2]);
+      paperX = toPx(args[1], null);
+      paperY = toPx(args[2], null);
       break;
     case 'linewidth':
-      lineWidth = toPx(args[1]);
+      lineWidth = toPx(args[1], null);
       break;
     case 'fontsize':
-      fontSize = toPx(args[1]);
+      fontSize = toPx(args[1], null);
       break;
     case 'text': {
       let t = args.slice(1).join(' ');
@@ -257,19 +313,22 @@ function render() {
       break;
     }
     case 'center':
-      centerX = toPx(args[1]);
-      centerY = toPx(args[2]);
+      centerX = toPx(args[1], null);
+      centerY = toPx(args[2], null);
+      break;
+    case 'distance':
+      distance = toPx(args[1], null);
       break;
     case 'ringscale':
       if (args.length > 2) {
-        ringScale = toPx(args[1]) / toPx(args[2]);
+        ringScale = toPx(args[1], null) / toPx(args[2], null);
       } else {
         ringScale = args[1];
       }
       break;
     case 'ringcaliber':
-      ringCaliberFrom = toPx(args[1]);
-      ringCaliberTo = toPx(args[2]);
+      ringCaliberFrom = toPx(args[1], null);
+      ringCaliberTo = toPx(args[2], null);
       break;
     case 'color':
       ringColor = args[1];
@@ -286,8 +345,16 @@ function render() {
       textColor = args[1];
       break;
     case 'ring': {
-      let d = (toPx(args[1]) + ringCaliberFrom / 2) * ringScale - ringCaliberTo / 2;
+      let d = toPxAdjusted(args[1], ringScale, distance, ringCaliberFrom, ringCaliberTo);
       let t = args.slice(2).join(' ');
+      let result = /(.*)@(\w+)/.exec(t);
+      if (result != null) {
+        let prefix = result[1];
+        let unit = result[2];
+        let unitValue = toPx('1' + unit, ringScale * distance);
+        let amount = d / unitValue;
+        t = result[1] + '@' + nicePrint(amount) + unit;
+      }
       let circle = document.createElementNS(ns, 'circle');
       circle.setAttribute('cx', centerX / unit);
       circle.setAttribute('cy', centerY / unit);
